851c08926ce299abbb751b9c52c95070509be633
jackielee <jackielee@192.168.1.3>
2019-06-06 18:24:43 +0800

    商品订单查询支持内部接口操作通过订单id查询

--- src/main/java/com/jeancoder/scm/internal/order/order_item_scm_list.groovy
+++ src/main/java/com/jeancoder/scm/internal/order/order_item_scm_list.groovy
@@ -1,9 +1,11 @@
 package com.jeancoder.scm.internal.order
 
+import com.jeancoder.app.sdk.JC
 import com.jeancoder.app.sdk.source.CommunicationSource
 import com.jeancoder.core.log.JCLogger
 import com.jeancoder.core.log.JCLoggerFactory
 import com.jeancoder.core.util.StringUtil
+import com.jeancoder.jdbc.JcTemplate
 import com.jeancoder.scm.ready.ajax.SimpleAjax
 import com.jeancoder.scm.ready.dto.order.OrderInfoDto
 import com.jeancoder.scm.ready.dto.order.OrderItemDto
@@ -14,9 +16,19 @@ import com.jeancoder.scm.ready.service.OrderService
 JCLogger Logger = JCLoggerFactory.getLogger(this.getClass().getName())
 List<OrderItemDto> list = new ArrayList<OrderItemDto>();
 try {
-	def order_no = StringUtil.trim((String)CommunicationSource.getParameter("order_no"));
-	OrderInfo order_info = new OrderInfo();
-	order_info = OrderService.__instance__.get_goods_order_info(order_no);
+	def order_no = JC.internal.param("order_no");
+	def order_id = JC.internal.param('order_id');
+	
+	OrderInfo order_info = null;
+	if(order_id) {
+		order_info = JcTemplate.INSTANCE().get(OrderInfo, 'select * from OrderInfo where id=?', order_id);
+	} else {
+		order_info = OrderService.__instance__.get_goods_order_info(order_no);
+	}
+	if(order_info==null) {
+		return SimpleAjax.notAvailable('商品订单未找到');
+	}
+	
 	OrderInfoDto orderinfodto = new OrderInfoDto();
 	orderinfodto.pay_amount = order_info.pay_amount;
 	List<OrderItem> order_items = new ArrayList<OrderItem>();
