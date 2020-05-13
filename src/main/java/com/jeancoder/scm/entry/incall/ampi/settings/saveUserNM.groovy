0b1370675ab9a0b575ad2307c714573a44aec2b8
jackielee <jackielee@192.168.1.2>
2020-05-13 10:30:37 +0800

    c

--- src/main/java/com/jeancoder/scm/entry/incall/ampi/settings/saveUserNM.groovy
+++ src/main/java/com/jeancoder/scm/entry/incall/ampi/settings/saveUserNM.groovy
@@ -1,8 +1,13 @@
 package com.jeancoder.scm.entry.incall.ampi.settings
 
 import com.jeancoder.app.sdk.JC;
+import com.jeancoder.core.log.JCLogger
+import com.jeancoder.core.log.JCLoggerFactory
 import com.jeancoder.scm.ready.incall.api.ProtObj;
 
 def request_data = new String(JC.request.get().getInputStream().getBytes(), 'UTF-8');
+
+JCLogger logger = JCLoggerFactory.getLogger('');
+logger.info(request_data);
 
 return ProtObj.success(1);
