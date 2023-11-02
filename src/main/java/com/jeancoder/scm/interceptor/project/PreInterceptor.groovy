5158fb52007a683540cd328aecc3298e351054a9
jackie lee <catiga03@gmail.com>
2023-11-02 14:21:46 +0800

    1

--- src/main/java/com/jeancoder/scm/interceptor/project/PreInterceptor.groovy
+++ src/main/java/com/jeancoder/scm/interceptor/project/PreInterceptor.groovy
@@ -15,7 +15,8 @@ String domain = req.getServerName();
 SysProjectInfo project = NativeUtil.connect(SysProjectInfo.class, 'project', '/incall/project', ["domain":domain]);
 GlobalHolder.setProj(project);
 req.setAttribute("current_project", project)
-req.setAttribute('pub_bucket', 'https://static.pdr365.com/static');
+// req.setAttribute('pub_bucket', 'https://static.pdr365.com/static');
+req.setAttribute('pub_bucket', 'http://static.jcloudapp.fancynote.vip/static/')
 
 req.setAttribute('str', '进销存');
 return true;
